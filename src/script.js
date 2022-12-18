e75db3055ef53fbbafa84caf34e9382df9277330
Bernard065 <bebenibernard@gmail.com>
2022-12-19 02:07:39 +0300

    fetch data from the public API

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // async function fetchProducts(url) {
-    //     const data = await fetch(url);
-    //     const response = await data.json();
-    //     console.log(response);
-    // }
-    // fetchProducts('https://api.escuelajs.co/api/v1/products');
+    async function fetchProducts(url) {
+        const data = await fetch(url);
+        const response = await data.json();
+        console.log(response);
+    }
+    fetchProducts('https://api.escuelajs.co/api/v1/products');
 });
