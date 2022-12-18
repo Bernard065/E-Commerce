72a58a21de6934ec93426081e406a6c0a2162b60
Bernard065 <bebenibernard@gmail.com>
2022-12-19 02:19:34 +0300

    create a variable product_info and assign it a value of the class product

--- src/script.js
+++ src/script.js
@@ -4,6 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
         const data = await fetch(url);
         const response = await data.json();
         //console.log(response);
+        const product_info = `
+            <div class="product">
+                <img src="" alt="" class="product-image">
+                <h2 class="product-title"></h2>
+                <h3 class="product-category"></h3>
+                <p class="product-description"></p>
+                <div class="product-price-container">
+                    <h3 class="product-price"></h3>
+                    <a href="" class="add-to-cart"></a>
+                </div>
+            </div>
+        `
     }
     //fetchProducts('https://api.escuelajs.co/api/v1/products');
 });
