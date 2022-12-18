9ed82b294baa959e562817265cd85c5bfdeac3b9
Bernard065 <bebenibernard@gmail.com>
2022-12-19 01:50:09 +0300

    link the html document to stlye.css and script.js

---
+++ src/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    async function fetchProducts(url) {
+        const data = await fetch(url);
+        const response = await data.json();
+        console.log(response);
+    }
+    fetchProducts('https://api.escuelajs.co/api/v1/products');
+});
