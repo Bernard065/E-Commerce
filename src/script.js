a5ae4c68a390abd42618e7def58f292159b26be4
Bernard065 <bebenibernard@gmail.com>
2022-12-19 02:11:51 +0300

    async function fetchProducts to fetch the products from the API

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //async function to fetch products from the public API
     async function fetchProducts(url) {
         const data = await fetch(url);
         const response = await data.json();
-        console.log(response);
+        //console.log(response);
     }
-    fetchProducts('https://api.escuelajs.co/api/v1/products');
+    //fetchProducts('https://api.escuelajs.co/api/v1/products');
 });
