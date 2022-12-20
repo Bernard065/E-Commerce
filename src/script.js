b2a2b57d324065f06a2bbc7b427ce3869d292c16
Bernard065 <bebenibernard@gmail.com>
2022-12-20 10:39:16 +0300

    iterating through the array using for loop

--- src/script.js
+++ src/script.js
@@ -1,11 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     //create variables
+    const url = `https://api.escuelajs.co/api/v1/products?offset=0&limit=5`
     const products = document.querySelector('.products');
-    
+    let dataset = 0;
+
     //async function to fetch products from the public API
     async function fetchProducts(url) {
-        const data = await fetch(url);
-        const response = await data.json();
+        try {
+            const data = await fetch(url);
+            dataset += 5;
+            if(data.ok) {
+                const response = await data.json();
+
+                for(let i = 0; i < response.length; i++) {
+                }
+
+            } 
+
+        }
+        
+        
         //console.log(response);
         const product_info = `
             <div class="product">
